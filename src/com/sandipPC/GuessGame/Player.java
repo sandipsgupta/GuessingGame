c9dd5c9fd97053507df822d9d4c5aec3b28aa927
sandip sengupta <sandipsgupta@gmail.com>
2018-09-25 23:28:20 -0500

    Updated println

--- src/com/sandipPC/GuessGame/Player.java
+++ src/com/sandipPC/GuessGame/Player.java
@@ -5,7 +5,7 @@ public class Player {
     public void guess() {
        // number = (int) (Math.random()*10);
        number = (int)(Math.random()*10);
-        System.out.println("I'm guessing"  + number);
+        System.out.println("I am guessing"  + number);
     }
 }
 
